fe1006ffa0b815dee4f306b14e35e9986202e06b
EDowning2000 <emersondowning@gmail.com>
2020-08-28 18:37:19 -0400

    added some basic things into the app so that i can see different options in the dropdown

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <div className="appHeader">
+        <h1>Covid tracker</h1>
+        <FormControl className="appDropdown">
+          <Select variant="outlined" value="test">
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
