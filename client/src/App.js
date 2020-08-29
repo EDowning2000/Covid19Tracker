e5b9dd00d45fc55f1d99141d1f0bb10dfcb97f4f
EDowning2000 <emersondowning@gmail.com>
2020-08-28 20:43:35 -0400

    got the countries and set them in the state to be used by the select mapping function

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState(["usa", "uk", "canada"]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+  }, []);
+
   return (
     <div className="App">
       <div className="appHeader">
         <h1>Covid tracker</h1>
         <FormControl className="appDropdown">
           <Select variant="outlined" value="test">
-            <MenuItem value="worldwide">WorldWide</MenuItem>
-            <MenuItem value="worldwide">WorldWide</MenuItem>
-            <MenuItem value="worldwide">WorldWide</MenuItem>
-            <MenuItem value="worldwide">WorldWide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
           </Select>
         </FormControl>
       </div>
